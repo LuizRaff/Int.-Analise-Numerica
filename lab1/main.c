5b1038dd8775ca175743a1a3a72997bb69ab3945
Luiz Raffaini <eduardoraffaini@MacBook-Air-de-Eduardo.local>
2025-03-20 11:37:42 -0300

    Lab1 completo

---
+++ lab1/main.c
@@ -0,0 +1,37 @@
+#include "taylor.h"
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#define PI 3.141592653589793
+
+int main (void)
+{
+  // escreva seu teste aqui
+  double x1 = 0.2617993878;
+  double y1 = avalia_seno(10, x1);
+  printf("Teste: x: %f (PI/12.0)", x1);
+  printf("Resultado: %f\n", y1);
+
+  double x2 = 0.3490658504;
+  double y2 = avalia_seno(10, x2);
+  printf("Teste: x: %f (PI/9.0)", x2);
+  printf("Resultado: %f\n", y2);
+
+  double x3 = 0.4487989505;
+  double y3 = avalia_seno(10, x3);
+  printf("Teste: x: %f (PI/7.0)", x3);
+  printf("Resultado: %f\n", y3);
+
+  double x4 = 0.7853981634;
+  double y4 = avalia_seno(10, x4);
+  printf("Teste: x: %f (PI/4.0)", x4);
+  printf("Resultado: %f\n", y4);
+
+  double x5 = 1.0471975512;
+  double y5 = avalia_seno(10, x5);
+  printf("Teste: x: %f (PI/3.0)", x5);
+  printf("Resultado: %f\n", y5);
+  
+  return 0;
+}
---
+++ lab1/taylor.c
@@ -0,0 +1,33 @@
+#include "taylor.h"
+#include <stdlib.h>
+
+double avalia_taylor (int n, double *c, double x0, double x){
+    if (n == 0){
+        return 0;
+    }
+
+    double fac = 1.0;
+    double delta = 1.0;
+    double fx = c[0];
+    for (int i = 1; i < n; i++)
+    {
+        delta *= (x - x0);
+        fac *= i;
+
+        fx += c[i] * (delta / fac);
+    }
+    
+    return fx;
+}
+
+double avalia_seno (int n, double x){
+    double cPossibilidades[4] = {0.0, 1.0, 0.0, -1.0};
+    double* coeficientes = (double*)malloc(n * sizeof(double));
+
+    for (int i = 0; i < n; i++)
+    {
+        coeficientes[i] = cPossibilidades[i%4];
+    }
+
+    return avalia_taylor(n, coeficientes, 0.0, x);
+}
